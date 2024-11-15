01dc7a995b21e6dc1795dd17c65a87ea23ab9206
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-15 15:30:16 -0800

    ran mkdir app/components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 
 export default function Home() {
+  console.log("What am I doing here? --SERVER/?");
   return (
       <h1 className="text-3xl">Welcome to next.</h1>
   );
